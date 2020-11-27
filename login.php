6689e2b7e9c7d7b812180562e482c8be46d5a270
Nicolas Faessel <nfaessel.ext@simplon.co>
2020-11-27 11:10:29 +0100

    Correction bug affichage du nom

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
         <title>Hey, dis moi qui tu es !</title>
     </head>
     <body>
-        <h1>Hello <?php echo $user['name']?></h1>
+        <h1>Hello <?php echo $user?></h1>
         <p><a href='suivant.php'>Continuer ?</a></p>
     </body>
 </html>
