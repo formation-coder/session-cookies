54b16e3a894840c1fea05e3798e35c85ba64563d
Nicolas Faessel <nfaessel.ext@simplon.co>
2020-11-27 11:27:32 +0100

    Users refactor, removed twig cache

--- twig-template.php
+++ twig-template.php
@@ -1,7 +1,5 @@
 <?php
 require_once './vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('templates');
-$twig = new \Twig\Environment($loader, [
-    'cache' => 'cache',
-]);
+$twig = new \Twig\Environment($loader);
 ?>
